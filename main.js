218f699ec80f005807cf253bb5fd61d6e05b3622
Tarık <feanorwastaken@gmail.com>
2022-08-26 18:21:17 +0300

    first

---
+++ main.js
@@ -0,0 +1,33 @@
+//haritalama
+const mymap = L.map('issMap').setView([0, 0], 2);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {attribution});
+tiles.addTo(mymap);
+
+//iss marker yapımı
+const issIcon = L.icon({
+iconUrl: 'iss.svg.png',
+iconSize: [100, 60],
+iconAnchor: [25, 16]
+});
+const marker = L.marker([0,0],{icon: issIcon}).addTo(mymap);
+
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+let FirstTime = true;
+
+async function getISS() {
+  const response = await fetch(api_url);
+  const data = await response.json();
+  const {latitude, longitude} = data;
+  if (FirstTime) {
+    mymap.setView([latitude, longitude], 2);
+    FirstTime = false;
+  }
+  marker.setLatLng([latitude, longitude]);
+
+  document.getElementById('lat').textContent = latitude.toFixed(2);
+document.getElementById('lon').textContent = longitude.toFixed(2);
+}
+getISS();
+setInterval(getISS,1000);
